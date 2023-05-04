5f6f8ae8e40167eea33e32215790d37b23ebc1e5
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2023-05-05 01:15:36 +0700

    feat: add Laptop

--- app/src/main/java/com/an/cuoiki/adapter/DienThoaiAdapter.java
+++ app/src/main/java/com/an/cuoiki/adapter/DienThoaiAdapter.java
@@ -1,6 +1,7 @@
 package com.an.cuoiki.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,7 +12,9 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.an.cuoiki.Interface.ItemClickListener;
 import com.an.cuoiki.R;
+import com.an.cuoiki.activity.ChiTietActivity;
 import com.an.cuoiki.model.SanPhamMoi;
 import com.bumptech.glide.Glide;
 
@@ -46,11 +49,22 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         if(holder instanceof MyViewHolder){
             MyViewHolder myViewHolder = (MyViewHolder) holder;
             SanPhamMoi sanPham = array.get(position);
-            myViewHolder.tensp.setText(sanPham.getTensp());
+            myViewHolder.tensp.setText(sanPham.getTensp().trim());
             DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
             myViewHolder.giasp.setText("Giá: "+ decimalFormat.format(Double.parseDouble(sanPham.getGiasp()))+" Đ");
             myViewHolder.mota.setText(sanPham.getMota());
             Glide.with(context).load(sanPham.getHinhanh()).into(myViewHolder.hinhanh);
+            myViewHolder.setItemClickListener(new ItemClickListener() {
+                @Override
+                public void onClick(View view, int pos, boolean isLongClick) {
+                    if (!isLongClick){
+                        //click
+                        Intent intent = new Intent(context, ChiTietActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        context.startActivity(intent);
+                    }
+                }
+            });
         }else {
             LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
             loadingViewHolder.progressBar.setIndeterminate(true);
@@ -77,9 +91,10 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
         }
     }
 
-    public class MyViewHolder extends RecyclerView.ViewHolder{
+    public class MyViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView tensp, giasp, mota;
         ImageView hinhanh;
+        private ItemClickListener itemClickListener;
 
         public MyViewHolder(@NonNull View itemView) {
             super(itemView);
@@ -87,7 +102,16 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHold
             giasp = itemView.findViewById(R.id.itemdt_gia);
             mota = itemView.findViewById(R.id.itemdt_mota);
             hinhanh = itemView.findViewById(R.id.itemdt_image);
+            itemView.setOnClickListener(this);
+        }
 
+        public void setItemClickListener(ItemClickListener itemClickListener) {
+            this.itemClickListener = itemClickListener;
+        }
+
+        @Override
+        public void onClick(View view) {
+            itemClickListener.onClick(view, getAdapterPosition(), false);
         }
     }
 }
