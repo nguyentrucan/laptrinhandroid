6fc0f355cc1e424ec4270429f37c582ce2822b8f
Nguyễn Trúc An <114329768+nguyentrucan@users.noreply.github.com>
2023-05-05 00:44:21 +0700

    feat: add Loading

--- app/src/main/java/com/an/cuoiki/activity/DienThoaiActivity.java
+++ app/src/main/java/com/an/cuoiki/activity/DienThoaiActivity.java
@@ -1,11 +1,13 @@
 package com.an.cuoiki.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Toast;
 
@@ -33,6 +35,9 @@ public class DienThoaiActivity extends AppCompatActivity {
     int loai;
     DienThoaiAdapter adapterDt;
     List<SanPhamMoi> sanPhamMoiList;
+    LinearLayoutManager linearLayoutManager;
+    Handler handler = new Handler();
+    boolean isLoading = false;
 
 
     @Override
@@ -44,19 +49,73 @@ public class DienThoaiActivity extends AppCompatActivity {
 
         Anhxa();
         ActionToolBar();
-        getData();
+        getData(page);
+        addEventLoad();
     }
 
-    private void getData() {
+    private void addEventLoad() {
+        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(@NonNull RecyclerView recyclerView, int newState) {
+                super.onScrollStateChanged(recyclerView, newState);
+            }
+
+            @Override
+            public void onScrolled(@NonNull RecyclerView recyclerView, int dx, int dy) {
+                super.onScrolled(recyclerView, dx, dy);
+                if (isLoading == false){
+                    if (linearLayoutManager.findLastCompletelyVisibleItemPosition() == sanPhamMoiList.size()-1){
+                        isLoading = true;
+                        loadMore();
+                    }
+                }
+            }
+        });
+    }
+
+    private void loadMore() {
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                sanPhamMoiList.add(null);
+                adapterDt.notifyItemInserted(sanPhamMoiList.size()-1);
+            }
+        });
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sanPhamMoiList.remove(sanPhamMoiList.size()-1);
+                adapterDt.notifyItemRemoved(sanPhamMoiList.size());
+                page = page + 1;
+                getData(page);
+                adapterDt.notifyDataSetChanged();
+                isLoading = false;
+            }
+        },2000);
+    }
+
+    private void getData(int page) {
         compositeDisposable.add(apiBanHang.getSanPham(page, loai)
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
                 .subscribe(
                         sanPhamMoiModel -> {
                             if (sanPhamMoiModel.isSuccess()){
-                                sanPhamMoiList = sanPhamMoiModel.getResult();
-                                adapterDt = new DienThoaiAdapter(getApplicationContext(), sanPhamMoiList);
-                                recyclerView.setAdapter(adapterDt);
+                                if(adapterDt == null){
+                                    sanPhamMoiList = sanPhamMoiModel.getResult();
+                                    adapterDt = new DienThoaiAdapter(getApplicationContext(), sanPhamMoiList);
+                                    recyclerView.setAdapter(adapterDt);
+                                }else {
+                                    int vitri = sanPhamMoiList.size()-1;
+                                    int soluongadd = sanPhamMoiModel.getResult().size();
+                                    for (int i=0;i<soluongadd;i++){
+                                        sanPhamMoiList.add(sanPhamMoiModel.getResult().get(i));
+                                    }
+                                    adapterDt.notifyItemRangeInserted(vitri, soluongadd);
+                                }
+                            }else {
+                                Toast.makeText(getApplicationContext(), "Hết dữ liệu", Toast.LENGTH_LONG).show();
+                                isLoading = true;
                             }
                         },
                         throwable -> {
@@ -79,8 +138,8 @@ public class DienThoaiActivity extends AppCompatActivity {
     private void Anhxa() {
         toolbar = findViewById(R.id.toolbar);
         recyclerView = findViewById(R.id.recycleview_dt);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
+        linearLayoutManager = new LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false);
+        recyclerView.setLayoutManager(linearLayoutManager);
         recyclerView.setHasFixedSize(true);
         sanPhamMoiList = new ArrayList<>();
     }
--- app/src/main/java/com/an/cuoiki/adapter/DienThoaiAdapter.java
+++ app/src/main/java/com/an/cuoiki/adapter/DienThoaiAdapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -17,9 +18,11 @@ import com.bumptech.glide.Glide;
 import java.text.DecimalFormat;
 import java.util.List;
 
-public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.MyViewHolder> {
+public class DienThoaiAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     Context context;
     List<SanPhamMoi> array;
+    private static final int VIEW_TYPE_DATA = 0;
+    private static final int VIEW_TYPE_LOADING = 1;
 
     public DienThoaiAdapter(Context context, List<SanPhamMoi> array) {
         this.context = context;
@@ -28,24 +31,50 @@ public class DienThoaiAdapter extends RecyclerView.Adapter<DienThoaiAdapter.MyVi
 
     @NonNull
     @Override
-    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai,parent,false);
-        return new MyViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        if (viewType == VIEW_TYPE_DATA){
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_dienthoai, parent, false);
+            return new MyViewHolder(view);
+        }else {
+            View view  = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_loading, parent, false);
+            return  new LoadingViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
-        SanPhamMoi sanPham = array.get(position);
-        holder.tensp.setText(sanPham.getTensp());
-        DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
-        holder.giasp.setText("Giá: "+ decimalFormat.format(Double.parseDouble(sanPham.getGiasp()))+" Đ");
-        holder.mota.setText(sanPham.getMota());
-        Glide.with(context).load(sanPham.getHinhanh()).into(holder.hinhanh);
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+        if(holder instanceof MyViewHolder){
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            SanPhamMoi sanPham = array.get(position);
+            myViewHolder.tensp.setText(sanPham.getTensp());
+            DecimalFormat decimalFormat = new DecimalFormat("###,###,###");
+            myViewHolder.giasp.setText("Giá: "+ decimalFormat.format(Double.parseDouble(sanPham.getGiasp()))+" Đ");
+            myViewHolder.mota.setText(sanPham.getMota());
+            Glide.with(context).load(sanPham.getHinhanh()).into(myViewHolder.hinhanh);
+        }else {
+            LoadingViewHolder loadingViewHolder = (LoadingViewHolder) holder;
+            loadingViewHolder.progressBar.setIndeterminate(true);
+        }
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return array.get(position) == null ? VIEW_TYPE_LOADING:VIEW_TYPE_DATA;
     }
 
     @Override
     public int getItemCount() {
         return array.size();
+    }
+
+
+    public class LoadingViewHolder extends RecyclerView.ViewHolder{
+        ProgressBar progressBar;
+
+        public LoadingViewHolder(@NonNull View itemView) {
+            super(itemView);
+            progressBar = itemView.findViewById(R.id.progressbar);
+        }
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder{
